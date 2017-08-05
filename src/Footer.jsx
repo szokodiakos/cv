ddf15f2d0bd93581efcf0f7f6023d1a1662737cc
Akos Szokodi <szokodiakos@gmail.com>
2017-08-05 10:07:54 +0200

    not printing footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,7 +5,7 @@ import './Footer.css';
 import data from './data.json';
 
 const Footer = () => (
-  <Grid className="cv-body">
+  <Grid className="cv-body no-print">
     <Row className="green-background">
       <div className="closing white-color small">
         {data.lastUpdated}
